f63689e8f125fd612c1c3eb62f06acfc518dbe11
fo40225 <fo40225@hotmail.com>
2015-08-08 16:10:52 +0800

    Tested

--- VisualStudioCodeCoverageResultConverter/Program.cs
+++ VisualStudioCodeCoverageResultConverter/Program.cs
@@ -2,14 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.Coverage.Analysis;
 
 namespace VisualStudioCodeCoverageResultConverter
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            if (args.Length == 0 || args.SelectMany(x => x).Contains('?'))
+            {
+                string help = @"usage: VisualStudioCodeCoverageResultConverter.exe /input:data.coverage /output:Result.coveragexml";
+                Console.WriteLine(help);
+            }
+            else
+            {
+                string input = args.Where(x => x.StartsWith("/input:", StringComparison.InvariantCultureIgnoreCase))
+                    .Select(x => Regex.Replace(x, "/input:", string.Empty, RegexOptions.IgnoreCase)).First();
+                string output = args.Where(x => x.StartsWith("/output:", StringComparison.InvariantCultureIgnoreCase))
+                    .Select(x => Regex.Replace(x, "/output:", string.Empty, RegexOptions.IgnoreCase)).First();
+                using (CoverageInfo info = CoverageInfo.CreateFromFile(input))
+                {
+                    info.BuildDataSet().ExportXml(output);
+                }
+            }
         }
     }
 }
