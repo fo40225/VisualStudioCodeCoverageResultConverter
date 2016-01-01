7fb660b59350a5d134e83c4c0126785c07c3d12e
fo40225 <fo40225@hotmail.com>
2016-01-01 14:50:41 +0800

    StringComparison.OrdinalIgnoreCase

--- VisualStudioCodeCoverageResultConverter/Program.cs
+++ VisualStudioCodeCoverageResultConverter/Program.cs
@@ -19,9 +19,9 @@ namespace VisualStudioCodeCoverageResultConverter
             }
             else
             {
-                string input = args.Where(x => x.StartsWith("/input:", StringComparison.InvariantCultureIgnoreCase))
+                string input = args.Where(x => x.StartsWith("/input:", StringComparison.OrdinalIgnoreCase))
                     .Select(x => Regex.Replace(x, "/input:", string.Empty, RegexOptions.IgnoreCase)).First();
-                string output = args.Where(x => x.StartsWith("/output:", StringComparison.InvariantCultureIgnoreCase))
+                string output = args.Where(x => x.StartsWith("/output:", StringComparison.OrdinalIgnoreCase))
                     .Select(x => Regex.Replace(x, "/output:", string.Empty, RegexOptions.IgnoreCase)).First();
                 using (CoverageInfo info = CoverageInfo.CreateFromFile(input))
                 {
